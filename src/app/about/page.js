bf0d278fcaaeb1849aad2d17265d44f280c25c1a
Rishab Raj <rr0948555@gmail.com>
2026-01-13 22:18:20 +0530

    Routing

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <>
+     <h2>About US</h2> 
+    </>
+  )
+}
+
+export default page
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+'use client'
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+   <main className={styles.main}>
+      <h2>Alex Pandian</h2>
+   </main>
+     
+  );
+}
