225ebaf8b0118a621a23ef8fb4b890e394f6f74c
Rishab Raj <rr0948555@gmail.com>
2026-01-14 21:52:54 +0530

    Nested Routing

---
+++ src/app/contact/employee/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const Employee = () => {
+  return (
+    <div>
+      <h2>Contact Employee</h2>
+      <Link href='/contact'>Go Back</Link>
+    </div>
+  )
+}
+
+export default Employee
